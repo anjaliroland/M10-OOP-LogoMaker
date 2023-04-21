a386a3243580260a83df3ad685b1f0d8f5e5594b
Anjali Roland <anjaligroland@gmail.com>
2023-04-20 17:00:17 -0700

    created inquirer prompts

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 const inquirer =require('inquirer')
+const fs = require('fs');
+const Shapes = require('./lib/shapes/Shapes');
+const Circle = require('./lib/shapes/Circle');
+const Square = require('./lib/shapes/Square');
+const Triangle = require('./lib/shapes/Triangle');
+const Colors = require('./lib/Colors');
+const Text = require('./lib/Text');
 
+const prompts =[
+    {
+        type: 'input',
+        name: 'text',
+        message: "What 3 letters do you want on your logo?"
+    },
+    {
+        type: 'input',
+        name: 'txtcolor',
+        message: "What color do you want the text to be?"
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: "Select what shape you want your logo to be.",
+        choices: ['Circle', 'Square', 'Triangle']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: "What color do you want the shape of your logo to be?"
+    },
+];
+
+const createSvg = (fileName, data) => {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log('Success! Enjoy your new logo!');
+        }
+   }) 
+};
+
+const init = () => {
+    inquirer
+    .prompt(prompts)
+    .then((ans) => createSvg('logo.svg', makeLogo(ans)))
+    .catch((err) => console.log(err));
+};
+
+init();
